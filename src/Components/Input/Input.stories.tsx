71ed790b79ac18e27a87815eb6fb9698284bc4d3
irina-lat <irochka.bars@mail.ru>
2022-08-21 23:24:45 +0300

    add story Input.stories.tsx

--- src/Components/Input/Input.stories.tsx
+++ src/Components/Input/Input.stories.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {ChangeEvent, useRef, useState} from 'react';
 import {ComponentStory, ComponentMeta} from '@storybook/react';
 
 
@@ -9,13 +9,27 @@ export default {
 }
 
 export const UncontrolledInput = () => <input/>
+
 export const TrackValueOfUncontrolledInput = () => {
     const [value, setValue] = useState('')
-    return <> <input onChange={(e) => {
+
+    const onChangeInput = (e: ChangeEvent<HTMLInputElement>) => {
         const actualValue = e.currentTarget.value;
         setValue((actualValue))
-    }}/> - {value} </>
+    }
+    return <> <input onChange={onChangeInput}/> - {value} </>
 }
 
-export const UncontrolledInputWithFixedValue = () => <input value={'it-incubator.by'}/>
+export const GetValueTrackValueOfUncontrolledInputByButton = () => {
+    const [value, setValue] = useState('')
+    const inputRef = useRef<HTMLInputElement>(null)
 
+    const save = () => {
+        const el = inputRef.current as HTMLInputElement
+        setValue(el.value)
+    }
+    return <> <input ref={inputRef}/>
+        <button onClick={save}> save </button> actual value - {value} </>
+}
+//
+// export const UncontrolledInputWithFixedValue= () => <input value={'it-incubator.by'} />
